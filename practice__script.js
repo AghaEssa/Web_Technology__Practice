63152afed89d53553729fc8561b74b6b59fb2829
Agha Essa khan <aghaessa22jky@gmail.com>
2025-06-07 20:31:35 -0700

    js practice basics

---
+++ practice__script.js
@@ -0,0 +1,149 @@
+
+let num1 = 5;
+let num2 = 10;
+let  sum = num1 + num2;
+console.log("  sum  of",  num1 , "and", num2 , "is =", sum);
+
+
+
+
+
+
+
+function multiply(num1,num2)
+{
+    return num1*num2;
+}
+console.log('multiplying ','12','and ','2 =',multiply(12,2))
+
+
+
+
+
+// this  is a single line comment
+
+/**
+ * this i multiple line comment
+ * explain the multiples 
+ */
+ 
+ 
+let valuee = 51; //assign 51 to variable valuee
+
+
+
+
+
+
+
+let  name  = "Agha Essa Khan"; 
+let age =  21;          
+let isStudent  =  true;  
+console.log( "Name :",  name);            
+console.log("Age :", age);                
+console.log("I am a Student =", isStudent);   
+
+
+
+
+
+
+
+const  pi =  3.14;
+let  radius  =  5;  
+let area = pi  * radius  * radius;
+console.log(" the area of the  circle with radius", radius, "is : "  , area);
+
+
+
+
+
+
+
+function operator(num1, num2) 
+{ 
+    let add = num1 + num2;
+    console.log(" addition:" , add );  
+ 
+    let sub = num1 - num2;
+    console.log(" subtraction:" , sub );  
+ 
+    let mul = num1 * num2;
+    console.log(" multiplication  " , mul ) ;  
+ 
+    let div = num1 / num2;
+        console.log(" division:", div); 
+
+}
+let num3 = 10;  
+let num4 = 5;  
+console.log(operator (num3 ,  num4));
+
+
+
+
+
+
+
+
+
+
+function calculate(num5) 
+{
+    let square = num5 * num5;
+    let cube = num5 * num5 * num5;
+    console.log(" number :", num5);
+    console.log(" square :" , square);
+    console.log(" cube : ",  cube);
+}
+let num5 = 4; 
+calculate ( num5) ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+let num6 = 10; 
+console.log(" initial value:", num6);  
+num6 += 5;  
+console.log("after += 5:", num6);  
+num6 -= 3; 
+console.log("After -= 3:", num6);  
+num6 *= 2;  
+console.log("After *= 2:", num6);  
+num6 /= 4;  
+console.log("After /= 4:", num6);  
+num6 %= 3;  
+console.log("After %= 3:", num6);  
+
+
+
+
+
+
+
+let Age = 30;  
+let Name = "Agha"; 
+let IsStudent = true;  
+let address;  
+let empty = null;  
+let person = {
+    name: "Bob",
+    age: 25
+};  
+let colors = ["red", "green", "blue"];  
+console.log("Type of age:", typeof Age);  
+console.log("Type of name:", typeof Name);  
+console.log("Type of isStudent:", typeof IsStudent);  
+console.log("Type of address:", typeof address);  
+console.log("Type of empty:", typeof empty);  
+console.log("Type of person:", typeof person);  
+console.log("Type of colors:", typeof colors);  
